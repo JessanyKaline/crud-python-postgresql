329bc1f28631bf1b90a9feec361b0172d58c9e76
Jessany Kaline <jessany42@gmail.com>
2023-02-09 23:01:39 -0300

    update

--- app.py
+++ app.py
@@ -7,88 +7,121 @@ app = Flask(__name__)
 
 @app.route("/create", methods=['POST'])
 def create():
-    body = request.json
-    categoria = body.get('categoria')
-    produto = body.get('produto')
-    quantidade = body.get('quantidade')
-    sql = "INSERT INTO produtos (categoria, produto, quantidade) VALUES (%s, %s, %s)"
-    connection = get_connection()
-    cursor = connection.cursor()
-    cursor.execute(sql, (categoria, produto, quantidade))
-    connection.commit()
-    cursor.close()
-    pool.putconn(connection)
+    try:
+        body = request.json
+        categoria = body.get('categoria')
+        produto = body.get('produto')
+        quantidade = body.get('quantidade')
+        sql = "INSERT INTO produtos (categoria, produto, quantidade) VALUES (%s, %s, %s)"
+        connection = get_connection()
+        cursor = connection.cursor()
+        cursor.execute(sql, (categoria, produto, quantidade))
+        connection.commit()
+        cursor.close()
+        pool.putconn(connection)
+        
+        return {"message": "Produto adicionado"}, 201
+    except Exception as e:
+        return {"message": str(e)}, 500
 
-    return {"message": "Produto adicionado"}, 201
 
 
 @app.route("/delete", methods=['DELETE'])
 def delete():
-    body = request.json
-    produto = body.get('produto')
-    sql = "DELETE FROM produtos WHERE produto like %s"
-    connection = get_connection()
-    cursor = connection.cursor()
-    cursor.execute(sql, (produto,))
-    connection.commit()
-    cursor.close()
-    pool.putconn(connection)
+    try:
+        body = request.json
+        produto = body.get('produto')
+        sql = "DELETE FROM produtos WHERE produto like %s"
+        connection = get_connection()
+        cursor = connection.cursor()
+        cursor.execute(sql, (produto,))
+        connection.commit()
+        cursor.close()
+        pool.putconn(connection)
+    except Exception as e:
+        return {"message": f"Erro ao deletar o produto: {str(e)}"}, 500
 
     return {"message": "Produto deletado"}, 200
 
 
+
 @app.route("/")
 def read_all():
-    sql = "select * from produtos;"
-    connection = get_connection()
-    cursor = connection.cursor()
-    cursor.execute(sql)
-    produtos = cursor.fetchall()
-    return produtos
+    try:
+        sql = "select * from produtos;"
+        connection = get_connection()
+        cursor = connection.cursor()
+        cursor.execute(sql)
+        produtos = cursor.fetchall()
+    except Exception as e:
+        return {"message": f"Erro ao ler os produtos: {str(e)}"}, 500
 
-
-@app.route("/read_one")
-def read_one():
-    body = request.json
-    produto = body.get('produto')
-    sql = "SELECT quantidade FROM produtos WHERE produto=%s;"
-    connection = get_connection()
-    cursor = connection.cursor()
-    cursor.execute(sql, (produto,))
-    resultado = cursor.fetchall()
-    return resultado
+    return produtos, 200
 
 
 @app.route("/update", methods=['PUT'])
 def update():
-    body = request.json
-    produto = body.get('produto')
-    quantidade = body.get('quantidade')
-    sql = "UPDATE produtos SET quantidade=%s WHERE produto=%s"
-    connection = get_connection()
-    cursor = connection.cursor()
-    cursor.execute(sql, [quantidade, produto])
-    connection.commit()
-    cursor.close()
-    pool.putconn(connection)
+    try:
+        body = request.json
+        produto = body.get('produto')
+        quantidade = body.get('quantidade')
+        sql = "UPDATE produtos SET quantidade=%s WHERE produto=%s"
+        connection = get_connection()
+        cursor = connection.cursor()
+        cursor.execute(sql, [quantidade, produto])
+        connection.commit()
+        cursor.close()
+        pool.putconn(connection)
 
-    return {"message": "Quantidade Atualizada"}, 200
+        return {"message": "Quantidade Total do Produto Atualizada"}, 200
+    except Exception as e:
+        return {"message": str(e)}, 500
+
 
 
 @app.route('/update_add', methods=['PUT'])
 def update_add():
-    produto_add = request.json['produto']
-    quantidade = request.json['quantidade']
-    sql= "SELECT quantidade FROM produtos WHERE produto = %s"
-    connection = get_connection()
-    cursor = connection.cursor()
-    cursor.execute(sql,[produto_add])
-    produto_quantidade = cursor.fetchone()[0]
-    produto_quantidade += quantidade
-    sql_add= "UPDATE produtos SET quantidade = %s WHERE produto = %s"
-    cursor.execute(sql_add, [produto_quantidade,produto_add,])
-    connection.commit()
-    cursor.close()
-    pool.putconn(connection)
 
-    return {"message": "Quantidade Acrescentada"}, 200
+    try:
+        produto_add = request.json['produto']
+        quantidade = request.json['quantidade']
+        sql = "SELECT quantidade FROM produtos WHERE produto = %s"
+        connection = get_connection()
+        cursor = connection.cursor()
+        cursor.execute(sql, [produto_add])
+        produto_quantidade = cursor.fetchone()[0]
+        produto_quantidade += quantidade
+        sql_add = "UPDATE produtos SET quantidade = %s WHERE produto = %s"
+        cursor.execute(sql_add, [produto_quantidade, produto_add,])
+        connection.commit()
+        cursor.close()
+        pool.putconn(connection)
+
+        return {"message": "Quantidade Acrescentada"}, 200
+
+    except Exception as e:
+        return {"message": "Erro ao atualizar a quantidade do produto: {}".format(e)}, 500
+
+@app.route('/update_sub', methods=['PUT'])
+def update_sub():
+    try:
+        produto_sub = request.json['produto']
+        quantidade = request.json['quantidade']
+        sql = "SELECT quantidade FROM produtos WHERE produto = %s"
+        connection = get_connection()
+        cursor = connection.cursor()
+        cursor.execute(sql, [produto_sub])
+        produto_quantidade = cursor.fetchone()[0]
+        if produto_quantidade < quantidade:
+            return {"message": "Não é possível subtrair a quantidade informada, pois é maior do que a quantidade atual do produto"}, 400
+        produto_quantidade -= quantidade
+        sql_sub = "UPDATE produtos SET quantidade = %s WHERE produto = %s"
+        cursor.execute(sql_sub, [produto_quantidade, produto_sub,])
+        connection.commit()
+        cursor.close()
+        pool.putconn(connection)
+
+        return {"message": "Quantidade Subtraída"}, 200
+
+    except Exception as e:
+        return {"message": "Erro ao subtrair a quantidade do produto: {}".format(e)}, 500
